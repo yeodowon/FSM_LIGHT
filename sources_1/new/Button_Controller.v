80a5ed9bcfd8886f6f0a71d89499f933491bf608
yeodowon <ehdnjsdk@gamil.com>
2022-10-13 15:27:08 +0900

    chatterting

--- sources_1/new/Button_Controller.v
+++ sources_1/new/Button_Controller.v
@@ -11,30 +11,43 @@ module Button_Controller(
               RELEASED = 1'b0,
               TRUE     = 1'b1,
               FALSE    = 1'b0;
+    parameter DEBOUNCE = 500_000; // 10ms for 100MHz
 
     reg r_prevState = RELEASED;
     reg r_button;
+    reg [31:0] r_counter = 0;
 
     assign o_button = r_button;
 
     always @(posedge i_clk or posedge i_reset) begin
         if (i_reset) begin
-            r_button <= RELEASED;
+            r_button    <= FALSE;
+            r_prevState <= RELEASED;
+            r_counter   <= 0;
         end        
         else begin
-            if ((i_button == PUSHED) && (r_prevState == RELEASED)) begin
+            if ((i_button == PUSHED) && (r_prevState == RELEASED) && (r_counter <DEBOUNCE)) begin
+                r_counter <= r_counter + 1;
+                r_button <= FALSE;
+            end
+            else if ((i_button == PUSHED) && (r_prevState == RELEASED) && (r_counter == DEBOUNCE)) begin
+                r_counter = 0;
                 r_prevState <= PUSHED;
                 r_button <= FALSE;
-        end
-        else begin
-            if ((i_button == RELEASED) && (r_prevState == PUSHED)) begin
+            end
+             else if ((i_button == RELEASED) && (r_prevState == PUSHED) && (r_counter < DEBOUNCE)) begin
+                r_counter <= r_counter + 1;
+                r_button <= FALSE;
+            end
+            else if ((i_button == RELEASED) && (r_prevState == PUSHED) && (r_counter == DEBOUNCE)) begin
+                r_counter = 0;
                 r_prevState <= RELEASED;
                 r_button <= TRUE;
-        end
+            end
+        
         else begin
             r_button <= FALSE;
         end
         end
-    end
     end
 endmodule
